6088df25f450a31f450de2fa53a3b91b76203122
Jordane <takoukam.jordane@gmail.com>
2024-08-25 21:15:00 +0100

    dialog list trousseau

--- src/components/TrousseauList.js
+++ src/components/TrousseauList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Typography, List, ListItem, ListItemText, Button } from '@mui/material';
+import { Box, Typography, List, ListItem, ListItemText, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { config } from '../_config/static_keys';
@@ -11,6 +11,8 @@ const TrousseauList = () => {
   const [trousseaux, setTrousseaux] = useState([]);
   const [loadingTrousseaux, setLoadingTrousseau] = useState(false);
   const [loadingId, setLoadingId] = useState(null); // ID du trousseau en cours de suppression
+
+
 
   const fetchTrousseaux = async () => {
     setLoadingTrousseau(true);
@@ -62,6 +64,21 @@ const TrousseauList = () => {
     }
   };
 
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [selectedTrousseau, setSelectedTrousseau] = useState(null);
+
+  const handleItemClick = (trousseau) => {
+    setSelectedTrousseau(trousseau);
+    setDialogOpen(true); // Ouvrir la boîte de dialogue lors du clic
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false); // Fermer la boîte de dialogue
+  };
+
+
+
   return (
     <Box sx={{ p: 3 }}>
       <Typography variant="h4" gutterBottom>
@@ -72,14 +89,14 @@ const TrousseauList = () => {
           loadingTrousseaux ?
             <Loader /> :
             trousseaux.length > 0 ? (
+
               trousseaux.map((trousseau, index) => (
-                <ListItem key={index}>
+                <ListItem key={index} style={{ cursor: 'pointer' }} onClick={() => handleItemClick(trousseau)}>
                   <ListItemText primary={trousseau.name} />
                   {loadingId === trousseau._id ? (
-                    <div style={{ height: "20px", width: "20px", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                      <Loader style={{ height: "100%", width: "100%" }} />
+                    <div style={{ height: '20px', width: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                      <Loader style={{ height: '100%', width: '100%' }} />
                     </div>
-
                   ) : (
                     <Button
                       onClick={(e) => {
@@ -95,10 +112,57 @@ const TrousseauList = () => {
                   )}
                 </ListItem>
               ))
+
+
             ) : (
               <Typography>Aucun trousseau trouvé.</Typography>
             )}
       </List>
+      <Dialog
+        open={dialogOpen}
+        onClose={handleDialogClose}
+        PaperProps={{
+          style: {
+            borderRadius: 15, // Arrondir les coins
+            padding: '20px',  // Ajouter un padding pour plus d'espace intérieur
+            maxWidth: '600px', // Augmenter la largeur maximale
+            width: '100%', // S'assurer que la boîte de dialogue occupe tout l'espace disponible
+          },
+        }}
+      >
+        <DialogTitle style={{ fontSize: '1.5em', fontWeight: 'bold', textAlign: 'center' }}>
+          Détails du Trousseau {selectedTrousseau?.name}
+        </DialogTitle>
+        <DialogContent>
+          {selectedTrousseau?.sharedWith.map((sharedUser, idx) => (
+            <div
+              key={sharedUser._id}
+              style={{
+                fontSize: '1.2em',
+                marginBottom: '15px',
+                padding: '10px ',
+                backgroundColor: '#f5f5f5',
+                borderRadius: '10px',
+              }}
+            >
+              {idx + 1} -     <strong> {idx === 0 ? `Moi (${sharedUser.email}) ` : sharedUser.email + " "}</strong> :
+              {sharedUser.permissions === 'read' ? ' Lecture' : sharedUser.permissions === 'write' ? ' Écriture' : ' Administrateur'}
+            </div>
+          ))}
+        </DialogContent>
+        <DialogActions style={{ justifyContent: 'end' }}>
+          <Button
+            onClick={handleDialogClose}
+            color="primary"
+            variant="contained"
+            style={{ borderRadius: '10px', padding: '5px 30px', margin: "0px 15px" }}
+          >
+            Fermer
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+
     </Box>
   );
 };
